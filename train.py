93cfadbe87de308d8c86f1d2d2a77f5e3993b319
aayush <ayushsahu3010@gmail.com>
2025-10-03 21:36:58 +0530

    changes by dvc

--- train.py
+++ train.py
@@ -1,9 +1,10 @@
 import random
 import sys
 from dvclive import Live
+import yaml
 
 with Live(save_dvc_exp=True) as live:
-    epochs = 10
+    epochs = yaml.safe_load(open("params.yaml"))["train"]["epochs"]
     live.log_param("epochs", epochs)
     for epoch in range(epochs):
         live.log_metric("train/accuracy", epoch + random.random())
