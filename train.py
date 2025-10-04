f68cfc5422ace95185d901ddf689850a2fc716f1
Aayush sahu <ayushsahu3010@gmail.com>
2025-10-04 22:35:10 +0530

    Update train.py

--- train.py
+++ train.py
@@ -6,9 +6,9 @@ import yaml
 with Live(save_dvc_exp=True) as live:
     epochs = yaml.safe_load(open("params.yaml"))["train"]["epochs"]
     live.log_param("epochs", epochs)
-    for epoch in range(epochs):
-        live.log_metric("train/accuracy", epoch + random.random())
-        live.log_metric("train/loss", epochs - epoch - random.random())
-        live.log_metric("val/accuracy",epoch + random.random() )
-        live.log_metric("val/loss", epochs - epoch - random.random())
-        live.next_step()
+    # for epoch in range(epochs):
+    #     live.log_metric("train/accuracy", epoch + random.random())
+    #     live.log_metric("train/loss", epochs - epoch - random.random())
+    #     live.log_metric("val/accuracy",epoch + random.random() )
+    #     live.log_metric("val/loss", epochs - epoch - random.random())
+    #     live.next_step()
